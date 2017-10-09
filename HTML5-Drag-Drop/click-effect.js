e781c6f70fa00f757fe1dbc3b05bb9e7228b1f63
unknown <xiagq@gildata.com>
2017-10-09 13:59:35 +0800

    backup

--- HTML5-Drag-Drop/click-effect.js
+++ HTML5-Drag-Drop/click-effect.js
@@ -9,6 +9,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
+        // 以次显示
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
@@ -33,3 +34,11 @@ jQuery(document).ready(function($) {
     });
 });
 // </script>
+
+/**
+ * https://laod.cn/wp-content/themes/begin/js/activate-power-mode.js
+ * https://laod.cn/registered
+ * 
+ * https://laod.cn/news/google-deeplearn-js.html
+ * https://research.googleblog.com/2017/08/harness-power-of-machine-learning-in.html
+ */
