a40fcb41cb0bc16b3e37a863959bd801a2c6405c
unknown <xiagq@gildata.com>
2017-10-09 19:47:00 +0800

    update

--- HTML5-Drag-Drop/index.js
+++ HTML5-Drag-Drop/index.js
@@ -1,39 +1,60 @@
 let idExist = false;
 
-function dragStart(ev) {
+function dragStart(e) {
+    console.log(`dragStart = `, e.target.id);
+    // dragLeave = boxA
     // console.log(`%c dragStart ................................`, `color: #f0f`);
-    // console.log(`ev.target.id = `, ev.target.id);
+    // console.log(`e.target.id = `, e.target.id);
     // boxA
-    let unique_name = `data-${ev.target.id}`;
-    let unique_value = `${ev.target.id}001`;
-    ev.target.setAttribute(unique_name, unique_value);
-    ev.dataTransfer.effectAllowed = 'move';
+    let unique_name = `data-${e.target.id}`;
+    let unique_value = `${e.target.id}001`;
+    e.target.setAttribute(unique_name, unique_value);
+    e.dataTransfer.effectAllowed = 'move';
     // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer#effectAllowed.28.29
     // copy, move ,link ,copyLink, copyMove, linkMove, all, none, or uninitialized.
-    ev.dataTransfer.setData("xtext", ev.target.getAttribute('id'));
+    e.dataTransfer.setData("xtext", e.target.getAttribute('id'));
     // id ??? xtext
-    // ev.dataTransfer.setDragImage(ev.target, 100, 100);
+    // e.dataTransfer.setDragImage(e.target, 100, 100);
     // console.log(`\n \n`);
     return true;
 }
 
+function dragLeave(e) {
+    console.log(`dragLeave = `, e.target.id);
+    // dragLeave = boxA
+    return true;
+}
 
-
-function dragEnd(ev) {
-    console.log(`dragEnd = `, ev.target.id);
+function dragEnd(e) {
+    console.log(`dragEnd = `, e.target.id);
     // dragEnd = boxA
+    console.log(`\n `);
     return true;
 }
 
 
-function dragDrop(ev) {
-    console.log(`%c dragDrop .............................`, `color: red`);
-    const id = ev.target.id || undefined;
-    // console.log(`ev.target.id = \n`, ev.target.id);
+function dragEnter(e) {
+    console.log(`dragEnter = `, e.target.id);
+    event.preventDefault();
+    return true;
+}
+
+function dragOver(e) {
+    console.log(`dragOver = `, e.target.id);
+    event.preventDefault();
+    return true;
+}
+
+
+function dragDrop(e) {
+    // console.log(`%c dragDrop .............................`, `color: red`);
+    const id = e.target.id || undefined;
+    // console.log(`e.target.id = `, e.target.id);
+    console.log(`dragDrop = `, e.target.id);
     // idBox
-    let data = ev.dataTransfer.getData("xtext");
-    console.log(`data / id = `, data);
-    let unique_name = `data-${ev.target.id}`;
+    let data = e.dataTransfer.getData("xtext");
+    // console.log(`data / id = `, data);
+    let unique_name = `data-${e.target.id}`;
     let low_unique_name = unique_name.substr(5).toLowerCase();
     // the attribute name after data- (note that dashes are converted to camelCase).
     // ??? camelCase ??? dashTocamelCase.js
@@ -46,18 +67,18 @@ function dragDrop(ev) {
     if (hasDiv !== null) {
         // ??? map() 
         let arr = idBox.querySelectorAll(`div`);
-        console.log(`arr = `, arr, typeof arr);
+        // console.log(`arr = `, arr, typeof arr);
         // object
-        console.log(`array ??? = `, Array.isArray(arr));
+        // console.log(`array ??? = `, Array.isArray(arr));
         // array ??? =  false
         for (let i = 0; i < arr.length; i++) {
-            console.log(`arr[i] = `, arr[i]);
+            // console.log(`arr[i] = `, arr[i]);
             idTest = arr[i].dataset[`${key}`];
             // 
             if (idTest) {
                 break;
             }
-            console.log(`idTest key = `, key);
+            // console.log(`idTest key = `, key);
         }
         /* arr.map(
             (div, index) => {
@@ -69,7 +90,7 @@ function dragDrop(ev) {
         // undefined / "boxA"
         // boxb
     }
-    console.log(`idTest = `, idTest);
+    // console.log(`idTest = `, idTest);
     // undefined
     if(idTest !== null && idTest !== undefined && idTest !== ``){
         idExist = true;
@@ -83,22 +104,15 @@ function dragDrop(ev) {
         // console.log(`data = \n`, data);
         // boxA
         let nid = data.toLowerCase();
-        ev.target.insertAdjacentHTML(`beforeend`, `<div data-${nid}="${data}"><h3>${data}</h3>😃 🙌 🎉 Tada/Hooray!</div>`);
+        e.target.insertAdjacentHTML(`beforeend`, `<div data-${nid}="${data}"><h3>${data}</h3>😃 🙌 🎉 Tada/Hooray!</div>`);
     }
-    console.log(`\n \n`);
-    ev.stopPropagation();
-    return false;
+    // console.log(`\n \n`);
+    e.stopPropagation();
+    // in case open new browser tab ???
+    return false;// over
 }
 
-function dragEnter(ev) {
-    event.preventDefault();
-    return true;
-}
 
-function dragOver(ev) {
-    event.preventDefault();
-    // return true;
-}
 
 
 
