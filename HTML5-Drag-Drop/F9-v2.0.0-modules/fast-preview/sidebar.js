9f5833d12db16e054102cbfb0b764fac27203c77
unknown <xiagq@gildata.com>
2017-10-20 20:11:30 +0800

    classList & attribute

---
+++ HTML5-Drag-Drop/F9-v2.0.0-modules/fast-preview/sidebar.js
@@ -0,0 +1,22 @@
+// h5-dnd-nav-box
+
+
+let lis = document.querySelectorAll(`.h5-dnd-nav-box`);
+
+let divs = document.querySelectorAll(`.h5-dnd-nav-box`);
+
+divs[0].attributes;
+
+// NamedNodeMap {0: class, length: 1}
+
+lis[0].addEventListener(`click`, () => {
+    // divs[0].getAttribute()
+    // divs[0].setAttribute()
+    div.classList.remove("foo");
+    div.classList.add("anotherclass");
+});
+
+
+
+
+
