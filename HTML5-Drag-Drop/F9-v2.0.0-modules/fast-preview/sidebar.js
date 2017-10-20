cddc015a7ff066ec31d57b8a055396af76186812
unknown <xiagq@gildata.com>
2017-10-20 20:19:46 +0800

    update

--- HTML5-Drag-Drop/F9-v2.0.0-modules/fast-preview/sidebar.js
+++ HTML5-Drag-Drop/F9-v2.0.0-modules/fast-preview/sidebar.js
@@ -5,15 +5,20 @@ let lis = document.querySelectorAll(`.h5-dnd-nav-box`);
 
 let divs = document.querySelectorAll(`.h5-dnd-nav-box`);
 
-divs[0].attributes;
+// divs[0].attributes;
 
 // NamedNodeMap {0: class, length: 1}
 
-lis[0].addEventListener(`click`, () => {
+lis[i].addEventListener(`click`, (i) => {
     // divs[0].getAttribute()
     // divs[0].setAttribute()
-    div.classList.remove("foo");
-    div.classList.add("anotherclass");
+    if (divs[i].hasAttribute("show")) {
+        divs[i].classList.remove("show");
+        divs[i].classList.add("hide");
+    }else{
+        ivs[i].classList.add("show");
+        divs[i].classList.remove("hide");
+    }
 });
 
 
