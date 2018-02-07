6d95da402d0fce5f796d95a5fab759185b8aa131
unknown <xiagq@gildata.com>
2018-02-07 10:36:15 +0800

    0 padding & format date

--- HTML5-Drag-Drop/F9-v2.0.0-modules/bonds-fv/000-xyz/datepicker/full-today.js
+++ HTML5-Drag-Drop/F9-v2.0.0-modules/bonds-fv/000-xyz/datepicker/full-today.js
@@ -14,9 +14,6 @@ const getFullTodayDate = (debug = false) => {
         month = date.getMonth() + 1,
         day = date.getDate();
         // week_daya = date.getDay();
-    // 0 padding
-    month = (month > 9) ? month : `0${month}`;
-    day = (day > 9) ? day : `0${day}`;
     let today = `${year}-${month}-${day}`;
     if (debug) {
         console.log(`today =`, today, typeof(today));
